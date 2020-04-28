379c2d113361f606b7bcd8c45053b7b9ea44eac3
colo6299 <rocketblox@gmail.com>
2020-04-28 12:56:31 +0900

    Add lambda comparison function

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -10,7 +10,7 @@ def is_sorted(list_in):
                 return False
     return True
 
-def bubble_sort(items, descending=False):
+def bubble_sort(items, descending=False, comparison=lambda a, b: a > b):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
@@ -19,23 +19,63 @@ def bubble_sort(items, descending=False):
     finished = False
     while finished is False:
         has_swapped = False
-        for index, item in enumerate(item):
+        for index, item in enumerate(items):
             if index is 0:
                 continue
             if descending:
-                if item > items[index-1]:  # What does DRY stand for, anyway?
+                if comparison(item, items[index-1]):  # What does DRY stand for, anyway?
                     items[index] = items[index-1]
                     items[index-1] = item
                     has_swapped = True
             else:
-                if item < items[index-1]:  # What does DRY stand for, anyway?
+                if comparison(items[index-1], item):  # What does DRY stand for, anyway?
                     items[index] = items[index-1]
                     items[index-1] = item
                     has_swapped = True
         if has_swapped is False:
             finished = True
 
-def selection_sort(items, descending=False):
+def cocktail_shaker_sort(items, descending=False, comparison=lambda a, b: a > b):
+    """Sort given items by shaking the list really hard.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+
+    finished = False
+    while finished is False:
+        has_swapped = False
+        for index, item in enumerate(items):
+            if index is 0:
+                continue
+            if descending:
+                if comparison(item, items[index-1]):   # What does DRY stand for, anyway?
+                    items[index] = items[index-1]
+                    items[index-1] = item
+                    has_swapped = True
+            else:
+                if comparison(items[index-1], item):   # What does DRY stand for, anyway?
+                    items[index] = items[index-1]
+                    items[index-1] = item
+                    has_swapped = True
+
+        for index in range(len(items) -1, -1, -1):
+            item = items[index]
+            if index is 0:
+                continue
+            if descending:
+                if comparison(item, items[index-1]):   # What does DRY stand for, anyway?
+                    items[index] = items[index-1]
+                    items[index-1] = item
+                    has_swapped = True
+            else:
+                if comparison(items[index-1], item):   # What does DRY stand for, anyway?
+                    items[index] = items[index-1]
+                    items[index-1] = item
+                    has_swapped = True
+
+        if has_swapped is False:
+            finished = True
+
+def selection_sort(items, descending=False, comparison=lambda a, b: a > b):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
@@ -45,7 +85,7 @@ def selection_sort(items, descending=False):
         for index in range(len(items)):
             maximum_ndx = index
             for p_index in range(index+1, len(items)):
-                if items[p_index] > items[maximum_ndx]:
+                if comparison(items[p_index], items[maximum_ndx]):
                     maximum_ndx = p_index
             items[index], items[maximum_ndx] = items[maximum_ndx], items[index]
 
@@ -53,26 +93,26 @@ def selection_sort(items, descending=False):
         for index in range(len(items)):
             minimum_ndx = index
             for p_index in range(index+1, len(items)):
-                if items[p_index] < items[minimum_ndx]:
+                if comparison(items[minimum_ndx], items[p_index]):
                     minimum_ndx = p_index
             items[index], items[minimum_ndx] = items[minimum_ndx], items[index]
 
-def insertion_sort(items, descending=False):
+def insertion_sort(items, descending=False, comparison=lambda a, b: a > b):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     
     for index in range(1, len(items)):
-        _insertion_helper(items, index, items[index], descending)
+        _insertion_helper(items, index, items[index], descending, comparison)
 
-def _insertion_helper(list_in, sublist_index, item, descending=False):
+def _insertion_helper(list_in, sublist_index, item, descending=False, comparison=lambda a, b: a > b):
     '''
     Handles insertion into sorted sublist during insertion sort. 
 
     sublist_index should equal working index.
     '''
-    insert_index = _binary_locator(list_in, 0, sublist_index, item, descending)
+    insert_index = _binary_locator(list_in, 0, sublist_index, item, descending, comparison)
     slide_index = sublist_index
     while slide_index is not insert_index and slide_index is not 0:
         list_in[slide_index] = list_in[slide_index-1]
@@ -80,7 +120,7 @@ def _insertion_helper(list_in, sublist_index, item, descending=False):
     list_in[insert_index] = item
 
 
-def _binary_locator(list_in, lower_index, upper_index, item, descending=False):
+def _binary_locator(list_in, lower_index, upper_index, item, descending=False, comparison=lambda a, b: a > b):
     '''
     Helper helper... recursive binary search for index
 
@@ -91,20 +131,20 @@ def _binary_locator(list_in, lower_index, upper_index, item, descending=False):
 
     if descending:
         center = (lower_index + upper_index) // 2  # stable for descending
-        if list_in[center] < item:
+        if comparison(item, list_in[center]):  # What does DRY stand for, anyway?
             upper_index = center - 1
-        elif list_in[center] == item:
-            return center
-        else:
+        elif comparison(list_in[center], item):
             lower_index = center
+        else:
+            return center     
 
     else:
         center = (lower_index + upper_index) // 2  # not stable for ascending, make it round up
-        if list_in[center] > item:
+        if comparison(list_in[center], item):  # What does DRY stand for, anyway?
             upper_index = center
-        elif list_in[center] == item:
+        elif list_in[center] == item:  
             return center
         else:
             lower_index = center + 1    
-            
+
     return _binary_locator(list_in, lower_index, upper_index, item, descending)
