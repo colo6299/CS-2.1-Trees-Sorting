c266332859e9b2fb885591df345774f1de227d6f
colo6299 <rocketblox@gmail.com>
2020-04-28 12:29:19 +0900

    Add insertion sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -15,7 +15,7 @@ def bubble_sort(items, descending=False):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    
+
     finished = False
     while finished is False:
         has_swapped = False
@@ -23,12 +23,12 @@ def bubble_sort(items, descending=False):
             if index is 0:
                 continue
             if descending:
-                if item > items[index-1]:
+                if item > items[index-1]:  # What does DRY stand for, anyway?
                     items[index] = items[index-1]
                     items[index-1] = item
                     has_swapped = True
             else:
-                if item < items[index-1]:
+                if item < items[index-1]:  # What does DRY stand for, anyway?
                     items[index] = items[index-1]
                     items[index-1] = item
                     has_swapped = True
@@ -57,12 +57,54 @@ def selection_sort(items, descending=False):
                     minimum_ndx = p_index
             items[index], items[minimum_ndx] = items[minimum_ndx], items[index]
 
-
-def insertion_sort(items):
+def insertion_sort(items, descending=False):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Take first unsorted item
-    # TODO: Insert it in sorted order in front of items
+    
+    for index in range(1, len(items)):
+        _insertion_helper(items, index, items[index], descending)
+
+def _insertion_helper(list_in, sublist_index, item, descending=False):
+    '''
+    Handles insertion into sorted sublist during insertion sort. 
+
+    sublist_index should equal working index.
+    '''
+    insert_index = _binary_locator(list_in, 0, sublist_index, item, descending)
+    slide_index = sublist_index
+    while slide_index is not insert_index and slide_index is not 0:
+        list_in[slide_index] = list_in[slide_index-1]
+        slide_index -= 1
+    list_in[insert_index] = item
+
+
+def _binary_locator(list_in, lower_index, upper_index, item, descending=False):
+    '''
+    Helper helper... recursive binary search for index
+
+    definitely going to need tweaking
+    '''
+    if upper_index == lower_index:
+        return upper_index
+
+    if descending:
+        center = (lower_index + upper_index) // 2  # stable for descending
+        if list_in[center] < item:
+            upper_index = center - 1
+        elif list_in[center] == item:
+            return center
+        else:
+            lower_index = center
+
+    else:
+        center = (lower_index + upper_index) // 2  # not stable for ascending, make it round up
+        if list_in[center] > item:
+            upper_index = center
+        elif list_in[center] == item:
+            return center
+        else:
+            lower_index = center + 1    
+            
+    return _binary_locator(list_in, lower_index, upper_index, item, descending)
